bc42431175d3f9609b06565b6f915053d612faa8
chenjw <512523616@qq.com>
2024-03-13 21:11:40 +0800

    优化

--- inbyte-component-app/inbyte-component-app-basic/src/main/java/com/inbyte/component/app/basic/SystemConfigUtil.java
+++ inbyte-component-app/inbyte-component-app-basic/src/main/java/com/inbyte/component/app/basic/SystemConfigUtil.java
@@ -1,7 +1,11 @@
 package com.inbyte.component.app.basic;
 
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import com.inbyte.commons.util.SpringContextUtil;
 import com.inbyte.component.app.basic.dao.SystemConfigMapper;
+
+import java.math.BigDecimal;
 
 /**
  * 系统配置工具类
@@ -17,7 +21,79 @@ public class SystemConfigUtil {
      * @param key
      * @return
      */
-    public String getValue(String key) {
+    public static String getValue(String key) {
         return SpringContextUtil.getBean(SystemConfigMapper.class).getValue(key);
     }
+
+    /**
+     * 获取系统配置值 String
+     *
+     * @param key
+     * @return
+     */
+    public static String getString(String key) {
+        return getValue(key);
+    }
+
+    /**
+     * 获取系统配置值 Integer 类型
+     *
+     * @param key
+     * @return
+     */
+    public static Integer getInteger(String key) {
+        return Integer.valueOf(getString(key));
+    }
+
+    /**
+     * 获取系统配置值 Long 类型
+     *
+     * @param key
+     * @return
+     */
+    public static Long getLong(String key) {
+        return Long.valueOf(getString(key));
+    }
+
+    /**
+     * 获取系统配置值 Double 类型
+     *
+     * @param key
+     * @return
+     */
+    public static Double getDouble(String key) {
+        return Double.valueOf(getString(key));
+    }
+
+    /**
+     * 获取系统配置值 BigDecimal 类型
+     *
+     * @param key
+     * @return
+     */
+    public static BigDecimal getBigDecimal(String key) {
+        return new BigDecimal(getString(key));
+    }
+
+    /**
+     * 获取系统配置值 JSONObject 类型
+     *
+     * @param key
+     * @return
+     */
+    public static JSONObject getJSONObject(String key) {
+        return JSONObject.parseObject(getString(key));
+    }
+
+    /**
+     * 获取系统配置值 JSONArray 类型
+     *
+     * @param key
+     * @return
+     */
+    public static JSONArray getJSONArray(String key) {
+        return JSONArray.parseArray(getString(key));
+    }
+
+
 }
