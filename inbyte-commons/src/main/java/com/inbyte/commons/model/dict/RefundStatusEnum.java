71a4900a5c4a03acf9e56eac78dd9263619ddd6b
chenjw <512523616@qq.com>
2024-06-18 21:37:08 +0800

    门票订单

---
+++ inbyte-commons/src/main/java/com/inbyte/commons/model/dict/RefundStatusEnum.java
@@ -0,0 +1,25 @@
+package com.inbyte.commons.model.dict;
+
+/**
+ * 退款状态
+ *
+ * @author chenjw
+ * @date 2022/10/19
+ */
+public enum RefundStatusEnum {
+
+    PENDING("待处理"),
+    APPROVED("已批准"),
+    COMPLETED("已完成"),
+    REJECTED("已拒绝");
+
+    private final String name;
+
+    RefundStatusEnum(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
