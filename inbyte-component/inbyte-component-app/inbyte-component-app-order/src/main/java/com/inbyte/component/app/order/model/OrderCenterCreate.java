62bbfc825a3cf6ffef02c2224c12f56ec2da1bd4
chenjw <512523616@qq.com>
2024-07-18 16:22:30 +0800

    优惠券用户，订单使用，退款等

--- inbyte-component/inbyte-component-app/inbyte-component-app-order/src/main/java/com/inbyte/component/app/order/model/OrderCenterCreate.java
+++ inbyte-component/inbyte-component-app/inbyte-component-app-order/src/main/java/com/inbyte/component/app/order/model/OrderCenterCreate.java
@@ -3,6 +3,7 @@ package com.inbyte.component.app.order.model;
 import com.alibaba.fastjson2.JSONObject;
 import com.inbyte.commons.model.enums.AppTypeEnum;
 import com.inbyte.commons.model.enums.OrderTypeEnum;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -21,54 +22,69 @@ import java.math.BigDecimal;
 public class OrderCenterCreate {
 
     /** 用户ID */
+    @NotNull(message = "用户ID不能为空")
     private Integer userId;
 
     /** 用户昵称 */
+    @NotNull(message = "用户昵称不能为空")
     private String nickname;
 
     /** 用户头像 */
     private String avatar;
 
     /** 用户电话 */
+    @NotNull(message = "用户电话不能为空")
     private String tel;
 
     /** 主要照片 */
+    @NotNull(message = "主要照片不能为空")
     private String mainPhoto;
 
     /** 订单编号 */
+    @NotNull(message = "订单编号不能为空")
     private String orderNo;
 
     /** 订单标题 */
+    @NotNull(message = "订单标题不能为空")
     private String orderTitle;
 
     /** 订单简介 */
+    @NotNull(message = "订单简介不能为空")
     private String orderBrief;
 
     /** 订单类型枚举 */
+    @NotNull(message = "订单类型不能为空")
     private OrderTypeEnum orderType;
 
     /** 订单扩展信息 */
     private JSONObject extent;
 
     /** 订单金额 */
+    @NotNull(message = "订单金额不能为空")
     private BigDecimal orderAmount;
 
     /** 应付金额 */
+    @NotNull(message = "应付金额不能为空")
     private BigDecimal payableAmount;
 
     /** 场馆ID */
+    @NotNull(message = "场馆ID不能为空")
     private String venueId;
 
     /** 场馆名称 */
+    @NotNull(message = "场馆名称不能为空")
     private String venueName;
 
     /** 商户号 */
+    @NotNull(message = "商户号不能为空")
     private String mctNo;
 
     /** 应用ID */
+    @NotNull(message = "应用ID不能为空")
     private String appId;
 
     /** 应用类型枚举 */
+    @NotNull(message = "应用类型不能为空")
     private AppTypeEnum appType;
 
 }
