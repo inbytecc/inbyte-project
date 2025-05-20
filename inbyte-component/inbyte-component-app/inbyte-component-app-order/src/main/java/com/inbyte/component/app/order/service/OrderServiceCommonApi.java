8c648f9369ed19a3c63379ac1570885b34a94ebe
chenjw <512523616@qq.com>
2025-05-20 13:56:54 +0800

    增加各环节订单轨迹，优惠券名称记录

--- inbyte-component/inbyte-component-app/inbyte-component-app-order/src/main/java/com/inbyte/component/app/order/service/OrderServiceCommonApi.java
+++ inbyte-component/inbyte-component-app/inbyte-component-app-order/src/main/java/com/inbyte/component/app/order/service/OrderServiceCommonApi.java
@@ -30,7 +30,7 @@ public interface OrderServiceCommonApi {
      * @param orderNo
      * @return
      */
-    R cancel(String orderNo, Integer userId);
+    R cancel(String orderNo, Integer userId, String reason);
 
     /**
      * 微信预支付
